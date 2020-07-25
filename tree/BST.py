57d63e8318c7905acec78b82d9690b022853f711
MkshSahani <msco18333@gmail.com>
2020-07-25 14:30:01 +0530

    Updated

--- tree/BST.py
+++ tree/BST.py
@@ -43,8 +43,28 @@ class BST:
     def inorderTraversal(self, node):
         if node is not None:
             self.inorderTraversal(node.left)
-            print(f"{node.key} ", end=" ")
+            print(f"{node.key} ", end='')
             self.inorderTraversal(node.right)
+
+    def preorderPrint(self):
+        self.preorderTraversal(self.root)
+
+    def preorderTraversal(self, node):
+        if node is not None:
+            print(f"{node.key} ", end='')
+            self.preorderTraversal(node.left)
+            self.preorderTraversal(node.right)
+
+    def height(self):
+        return self.getheight(self.root)
+    
+    def getheight(self, node):
+        if node is None:
+            return -1
+        else:
+            rh = self.getheight(node.right) + 1
+            lh = self.getheight(node.left) + 1
+            return rh if rh > lh else lh 
 
 
 
@@ -56,4 +76,6 @@ if __name__ == '__main__':
     print(bst.root)
     bst.printTree()
     print()
-        
+    bst.preorderPrint()
+    print()
+    print("Height of the Binary Search Tree : ", bst.height())
---
+++ tree/Stack.py
@@ -0,0 +1,27 @@
+# Stack.py 
+
+class Stack:
+    
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, element):
+        self.stack.append(element)
+    
+    def pop(self):
+        if self.isempty():
+            raise ValueError("Stack UnderFlow")
+        else:
+            return self.stack.pop(0)
+
+    def isempty(self):
+        return self.stack == []
+
+    def top(self):
+        if self.isempty():
+            raise ValueError("Stack underflow")
+        else:
+            return self.stack[0]
+
+    
