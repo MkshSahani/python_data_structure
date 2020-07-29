16d95ab3b817645c3e9db6f696a2c70bc505cea4
MkshSahani <msco18333@gmail.com>
2020-07-29 17:40:27 +0530

    Updated

--- list/singlyLinkedList.py
+++ list/singlyLinkedList.py
@@ -27,16 +27,32 @@ class List:
 
     def printList(self):
         temp = self.head
-        print("List : ")
+        print("List : ", end = '')
         while temp.link is not None:
             print(temp.key, end = ' ')    
             temp = temp.link
-            
+        print()
 
+    def deleteNode(self, key):
+        temp = self.head
+        previous = temp
+        while temp is not None and temp.key != key:
+            previous = temp
+            temp = temp.link
+        
+        if temp is not None:
+            if temp == self.head:
+                self.head = self.head.link
+            else:
+                previous.link = temp.link 
 
 if __name__ == '__main__':
     # singlyLinkedList implementation
     list = List()
-    for i in range(10):
+    for i in range(30):
         list.insertNode(random.randint(1, 100))
-    list.printList() # print the list. 
+    list.printList()  # print the list.
+    for i in range(4):
+        key = int(input())
+        list.deleteNode(key)
+        list.printList() # print the modified list. 
